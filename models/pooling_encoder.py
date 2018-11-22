7d6d7fded464dea905272836e5c752bc82694c7d
michael <michael_ball@brown.edu>
2018-11-22 16:52:53 -0500

    minor improvements

--- models/pooling_encoder.py
+++ models/pooling_encoder.py
@@ -6,14 +6,20 @@ from torch.autograd import Variable
 
 class PoolingEncoder(nn.Module):
     def __init__(self, pooling_type="max"):
+        """
+        Encodes sentences by pooling the word embeddings of 
+            the tokens in the sentence using a given pooling
+            method.
+        Args:
+            pooling_type (str): specifies which type of pooling
+        """
         super().__init__()
         self.pooling_type = pooling_type
 
     def forward(self, input):
-        x = Variable(torch.tensor(input, dtype=torch.float), requires_grad=True)
         if self.pooling_type == "max":
-            output, _ = torch.max(x, 0)
+            output, _ = torch.max(input, 0)
         elif self.pooling_type == "ave":
-            output, _ = torch.mean(x, 0)
+            output, _ = torch.mean(input, 0)
         
-        return output
+        return output.reshape(1, -1)
