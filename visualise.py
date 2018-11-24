8be75b01191a8100a5561762fff3ecae7b28716b
michael <michael_ball@brown.edu>
2018-11-23 19:56:00 -0500

    made visualisation more configurable

--- visualise.py
+++ visualise.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
 
-def plot_train_test_loss(train_losses, test_losses):
+def plot_train_test_loss(train_losses, test_losses, display=True, save_file=None):
     fig, ax = plt.subplots()
     ax.plot(train_losses, 'b-', label='train loss')
     ax.plot(test_losses, 'r-', label='test loss')
@@ -12,14 +12,20 @@ def plot_train_test_loss(train_losses, test_losses):
     ax.set(xlabel='epoch number', ylabel='average loss', title='train/test losses')
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.grid()
-    plt.show()
+    if save_file:
+        plt.savefig(save_file)
+    if display:
+        plt.show()
 
 
-def plot_train_test_accs(train_accs, test_accs):
+def plot_train_test_accs(train_accs, test_accs, display=True, save_file=None):
     fig, ax = plt.subplots()
     ax.plot(train_accs, 'b-', label='train accuracy')
     ax.plot(test_accs, 'r-', label='test accuracy')
     ax.set(xlabel='epoch number', ylabel='accuracy', title='train/test accuracies')
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.grid()
-    plt.show()
+    if save_file:
+        plt.savefig(save_file)
+    if display:
+        plt.show()
