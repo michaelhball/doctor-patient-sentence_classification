c3daddc056baedbfa4c1fe6967335836111b293c
michael <michael_ball@brown.edu>
2018-11-24 18:42:26 -0500

    final changes to writer

--- data_writer.py
+++ data_writer.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 from pathlib import Path
 
@@ -18,8 +19,8 @@ class DataWriter():
         self.simple_c, self.extended_c = simple_classifier, extended_classifier
         self.dr = DataReader(test_tsv)
         self.di = self.simple_c.test_di
-        self.column_headings = self.dr.col_headings
         self.tsv_data = self.dr.read()
+        self.column_headings = self.dr.col_headings
         self.formatted_data = self.di.data
         self.output_file = output_file
         self.output_data = None
@@ -31,17 +32,22 @@ class DataWriter():
             simple_output = self.simple_c.classify(x2)
             simple_class = simple_output.argmax(0)
             simple_class_str = self.di.simple_labels[simple_class]
-            simple_confidence = simple_output[simple_class]
+            simple_confidence = round(self.softmax(simple_output)[simple_class], 3)
             extended_output = self.extended_c.classify(x2)
             extended_class = extended_output.argmax(0)
             extended_class_str = self.di.extended_labels[extended_class]
-            extended_confidence = extended_output[extended_class]
-            confidences = "simple: {0}, extended: {1}".format(round(simple_confidence, 3), round(extended_confidence, 3))
-            self.output_data.append(x1[:6] + [simple_class_str, extended_class_str, confidences])
+            extended_confidence = round(self.softmax(extended_output)[extended_class], 3)
+            confidences = "simple: {0}, extended: {1}".format(simple_confidence, extended_confidence)
+            row = x1[:6] + [simple_class_str, extended_class_str, confidences]
+            self.output_data.append('\t'.join(row))
+
+    def softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
 
     def write(self):
         if not self.output_data:
             self.create_output_data()
-        with Path(self.output_file).open('wb') as f:
+        with Path(self.output_file).open('w') as f:
             writer = csv.writer(f)
             writer.writerows(self.output_data)
