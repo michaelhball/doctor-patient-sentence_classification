6ae0294ef1cd11202b8cf589358ce7c7c40462e2
michael <michael_ball@brown.edu>
2018-11-24 15:24:18 -0500

    cleaned up preprocessing class - do not need the complexity

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,12 @@
-import pickle
 import spacy
-import string
 
 from collections import Counter, defaultdict
-from pathlib import Path
 
 
 def tokenise(data):
+    """
+    Tokenises input data (each row is one example from dataset).
+    """
     tokeniser = spacy.load('en')
     tokenised_data = []
     for example in data:
@@ -18,47 +18,22 @@ def tokenise(data):
 
 
 def tokenise_sentence(tokeniser, sent):
-    tokenised = [t.text.lower() for t in tokeniser(sent)]
-    
-    # REALISED THAT WORD EMBEDDINGS HAVE THESE CONTRACTION ENDINGS SO NO NEED TO DO THIS FOR NOW, JUST USE EMBEDDING
-    # deleted_inds = []
-    # for i, token in enumerate(tokenised):
-        
-        # if token.startswith("'"):
-        #     tokenised[i-1] += token
-        #     deleted_inds.append(i)
-        # if token == "n't":
-        #     tokenised[i] = "not"
-
-    # num_deleted = 0
-    # for j in deleted_inds:
-    #     del tokenised[j - num_deleted]
-    #     num_deleted += 1
-
-    return [t for t in tokenised if t not in string.punctuation]
+    """
+    Tokenises sentence using Spacy tokeniser (found to work well even for 
+        poorly-formed English sentences)
+    """
+    return [t.text.lower() for t in tokeniser(sent)]
 
 
 def create_vocab(sentences):
+    """
+    Creates a vocab (and the inverse dictionary) given a 
+        list of tokenised sentences.
+    """
     freq = Counter(p for o in sentences for p in o)
-    vocab = [o for o, c in freq.most_common(5000)]
+    vocab = [o for o, c in freq.items()]
     vocab.insert(0, '_pad_')
     vocab.insert(0, '_unk_')
     string2idx = defaultdict(lambda:0, {v: k for k, v in enumerate(vocab)})
 
     return vocab, string2idx
-    
-
-if __name__ == "__main__":
-    t = tokenise_sentence(spacy.load('en'), "Errr, yeah, I haven't been to the loo for quite a time, quite a while, maybe 3 or 4 days")
-#     t = tokenise_sentence(spacy.load('en'), "Umm, it's both my hands")
-    # print(t)
-
-    # data = pickle.load(Path('./data/data.pkl').open('rb'))
-    # tokenised_data = tokenise(data)
-    # pickle.dump(tokenised_data, Path('./data/data_tokenised.pkl').open('wb'))
-
-    # tokenised_data = pickle.load(Path('./data/data_tokenised.pkl').open('rb'))
-    # sentences = [r[3] for r in tokenised_data]
-    # vocab, string2idx = create_vocab(sentences)
-
-    
